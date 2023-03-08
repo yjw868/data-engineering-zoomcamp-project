06eec0bc9cf9ce9b67885c293e0baf13d0f28ea3
yjw868 <yjwang868@gmail.com>
2023-03-08 15:13:04 +0000

    made conection with schema-registry

---
+++ README.md
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+### References
+- [Stomp - The Simple Text Oriented Messaging Protocol](https://stomp.github.io/index.html)
+- [Python Rest Client Schema Registry](https://marcosschroh.github.io/python-schema-registry-client/)
+- [Train Movement wiki](https://wiki.openraildata.com/index.php?title=Train_Movement)
+- []()
+- []()
+- []()
+- []()
+- 
+### Credits
+- [On Track with Apache Kafka – Building a Streaming ETL Solution with Rail Data](https://www.confluent.io/en-gb/blog/build-streaming-etl-solutions-with-kafka-and-rail-data/)
+- [KaitaiD / py-network-rail-feeder](https://github.com/KaitaiD/py-network-rail-feeder)
+- [zkan / dtc-data-engineering-zoomcamp-project](https://github.com/zkan/dtc-data-engineering-zoomcamp-project)
+- [openraildata / td-trust-example-python3](https://github.com/openraildata/td-trust-example-python3)
+- []()
+- []()
+- []()
+- []()
---
+++ ingest/create_schema.py
@@ -0,0 +1,133 @@
+from schema_registry.client import SchemaRegistryClient, schema
+
+# from confluent_kafka.schema_registry import SchemaRegistryClient, schema
+
+client = SchemaRegistryClient(url='http://host.docker.internal:8081')
+# client.update_compatibility(level="NONE", subject="TRAIN_MVT_ALL_TOC")
+print(client.get_subjects())
+deployment_schema = {
+    "type": "record",
+    "namespace": "dtc-project",
+    "name": "value_TRAIN_MVT_ALL_TOC",
+    "fields": [
+    {
+      "name": "event_type",
+      "type": "string"
+    },
+    {
+      "name": "gbtt_timestamp",
+      "type": "string"
+    },
+    {
+      "name": "original_loc_stanox",
+      "type": "string"
+    },
+    {
+      "name": "planned_timestamp",
+      "type": "string"
+    },
+    {
+      "name": "timetable_variation",
+      "type": "string"
+    },
+    {
+      "name": "original_loc_timestamp",
+      "type": "string"
+    },
+    {
+      "name": "current_train_id",
+      "type": "null"
+    },
+    {
+      "name": "delay_monitoring_point",
+      "type": "string"
+    },
+    {
+      "name": "next_report_run_time",
+      "type": "string"
+    },
+    {
+      "name": "reporting_stanox",
+      "type": "string"
+    },
+    {
+      "name": "actual_timestamp",
+      "type": "string"
+    },
+    {
+      "name": "correction_ind",
+      "type": "string"
+    },
+    {
+      "name": "event_source",
+      "type": "string"
+    },
+    {
+      "name": "train_file_address",
+      "type": "string"
+    },
+    {
+      "name": "platform",
+      "type": "string"
+    },
+    {
+      "name": "division_code",
+      "type": "string"
+    },
+    {
+      "name": "train_terminated",
+      "type": "string"
+    },
+    {
+      "name": "train_id",
+      "type": "string"
+    },
+    {
+      "name": "offroute_ind",
+      "type": "string"
+    },
+    {
+      "name": "variation_status",
+      "type": "string"
+    },
+    {
+      "name": "train_service_code",
+      "type": "string"
+    },
+    {
+      "name": "toc_id",
+      "type": "string"
+    },
+    {
+      "name": "loc_stanox",
+      "type": "string"
+    },
+    {
+      "name": "auto_expected",
+      "type": "string"
+    },
+    {
+      "name": "direction_ind",
+      "type": "string"
+    },
+    {
+      "name": "route",
+      "type": "string"
+    },
+    {
+      "name": "planned_event_type",
+      "type": "string"
+    },
+    {
+      "name": "next_report_stanox",
+      "type": "string"
+    },
+    {
+      "name": "line_ind",
+      "type": "string"
+    }
+  ]
+}
+
+avro_schema = schema.AvroSchema(deployment_schema)
+schema_id = client.register("test-deployment", avro_schema)
