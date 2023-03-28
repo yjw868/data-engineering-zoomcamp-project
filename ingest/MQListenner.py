62989bde71df128851ef1e1cf37966b7ea7976c7
yjw868 <yjwang868@gmail.com>
2023-03-29 00:57:46 +0100

    refactoring

---
+++ ingest/MQListenner.py
@@ -0,0 +1,79 @@
+import json
+from datetime import datetime
+
+import stomp
+from pytz import timezone
+
+TIMEZONE_LONDON: timezone = timezone("Europe/London")
+
+MESSAGES = {
+    "0001": "activation",
+    "0002": "cancellation",
+    "0003": "movement",
+    "0004": "_unidentified",
+    "0005": "reinstatement",
+    "0006": "origin change",
+    "0007": "identity change",
+    "0008": "_location change",
+}
+
+
+class MQListener(stomp.ConnectionListener):
+    def __init__(self, messager, topic, producer, streaming=False):
+        self.msg = messager
+        self.topic = topic
+        self.producer = producer
+        self.streaming = streaming
+
+    def print_message(self, message_raw):
+        message = json.loads(message_raw)
+        for a in message:
+            body = a["body"]
+
+            toc = a["body"].get("toc_id", "")
+            platform = a["body"].get("platform", "")
+            loc_stanox = "@" + body.get("loc_stanox", "")
+            timestamp = int(a["body"].get("actual_timestamp", 0)) / 1000
+            utc_datetime = datetime.utcfromtimestamp(timestamp)
+            uk_datetime = TIMEZONE_LONDON.fromutc(utc_datetime)
+            variation_status = a["body"].get("variation_status", "")
+
+            summary = "{} ({} {}) {:<13s} {:2s} {:<6s} {:3s} {} {}".format(
+                body["train_id"],
+                body["train_id"][2:6],
+                body["train_id"][6],
+                MESSAGES[a["header"]["msg_type"]],
+                toc,
+                loc_stanox,
+                platform,
+                uk_datetime,
+                variation_status,
+            )
+            # print(body)
+            print(summary)
+
+    def on_message(self, frame):
+        """Stomp message handler"""
+
+        headers, message_raw = frame.headers, frame.body
+
+        # Acknowledging messages is important in client-individual mode and keep a durable subscription
+        self.msg.ack(id=headers["message-id"], subscription=headers["subscription"])
+
+        if self.streaming:
+            # print(frame)
+            msg = json.loads(message_raw)
+            # print(msg)
+            # self.producer.produce(self.topic, json.dumps(msg))
+            for info in msg:
+                header = info["header"]
+                msg_type = header["msg_type"]
+                body = info["body"]
+                if msg_type == "0003":
+                    # print(type(body))
+                    train_records = self.producer.read_records([body])
+                    self.producer.publish(
+                        topic="train_movements", records=train_records
+                    )
+        else:
+            self.print_message(message_raw)
--- ingest/datafeeder_avro.py
+++ ingest/datafeeder_avro.py
@@ -1,192 +1,16 @@
-import json
-import os
-import socket
-from datetime import datetime
 from time import sleep
-from typing import Dict
 
 import stomp
 import toml
-from confluent_kafka import Producer
-from confluent_kafka.schema_registry import SchemaRegistryClient
-from confluent_kafka.schema_registry.avro import AvroSerializer
-from confluent_kafka.serialization import MessageField, SerializationContext
-from pytz import timezone
-from train_record import TrainRecord, train_record_to_dict
-from train_record_key import TrainRecordKey, train_record_key_to_dict
+from MQListenner import MQListener
+from train_avro_producer import TrainAvroProducer
 
 with open("../secrets.toml", "r") as f:
     secrets = toml.load(f)
 
-TIMEZONE_LONDON: timezone = timezone("Europe/London")
-
 # Message type
-MESSAGES = {
-    "0001": "activation",
-    "0002": "cancellation",
-    "0003": "movement",
-    "0004": "_unidentified",
-    "0005": "reinstatement",
-    "0006": "origin change",
-    "0007": "identity change",
-    "0008": "_location change",
-}
 # Datafeeds for information on the Open Data available from the rail industry in Great Britain
 HOSTNAME = "datafeeds.networkrail.co.uk"
-
-
-# def create_producer():
-#     # topic = "public.networkrail.movement"
-#     conf = {
-#         "bootstrap.servers": "host.docker.internal:9092",
-#         "client.id": socket.gethostname(),
-#     }
-#     producer = Producer(conf)
-#     return producer
-
-
-# producer = create_producer()
-
-
-def delivery_report(err, msg):
-    if err is not None:
-        print("Delivery failed for record {}: {}".format(msg.key(), err))
-        return
-    print(
-        "Record {} successfully produced to {} [{}] at offset {}".format(
-            msg.key(), msg.topic(), msg.partition(), msg.offset()
-        )
-    )
-
-
-class TrainAvroProducer:
-    def __init__(self, props: Dict):
-        # Schema Registry and Serializer-Deserializer Configurations
-        key_schema_str = self.load_schema(props["schema.key"])
-        value_schema_str = self.load_schema(props["schema.value"])
-        schema_registry_props = {"url": props["schema_registry.url"]}
-        schema_registry_client = SchemaRegistryClient(schema_registry_props)
-        self.key_serializer = AvroSerializer(
-            schema_registry_client, key_schema_str, train_record_key_to_dict
-        )
-        self.value_serializer = AvroSerializer(
-            schema_registry_client, value_schema_str, train_record_to_dict
-        )
-
-        # Producer Configuration
-        producer_props = {"bootstrap.servers": props["bootstrap.servers"]}
-        self.producer = Producer(producer_props)
-
-    @staticmethod
-    def load_schema(schema_path: str):
-        # path = os.path.realpath(os.path.dirname(__file__))
-        # with open(f"{path}/{schema_path}") as f:
-        with open(f"{schema_path}") as f:
-            schema_str = f.read()
-        return schema_str
-
-    @staticmethod
-    def delivery_report(err, msg):
-        if err is not None:
-            print("Delivery failed for record {}: {}".format(msg.key(), err))
-            return
-        print(
-            "Record {} successfully produced to {} [{}] at offset {}".format(
-                msg.key(), msg.topic(), msg.partition(), msg.offset()
-            )
-        )
-
-    @staticmethod
-    def read_records(reader: list):
-        train_records, train_keys = [], []
-        for row in reader:
-            train_records.append(TrainRecord.from_dict(row))
-            train_keys.append(TrainRecordKey.from_dict(row))
-        return zip(train_keys, train_records)
-
-    def publish(self, topic: str, records: [TrainRecordKey, TrainRecord]):
-        for key_value in records:
-            key, value = key_value
-            try:
-                self.producer.produce(
-                    topic=topic,
-                    key=self.key_serializer(
-                        key, SerializationContext(topic=topic, field=MessageField.KEY)
-                    ),
-                    value=self.value_serializer(
-                        value,
-                        SerializationContext(topic=topic, field=MessageField.VALUE),
-                    ),
-                    on_delivery=delivery_report,
-                )
-            except KeyboardInterrupt:
-                break
-            except Exception as e:
-                print(f"Exception while producing record - {value}: {e}")
-
-        self.producer.flush()
-        sleep(10)
-
-
-class MVListener(stomp.ConnectionListener):
-    def __init__(self, messager, topic, producer, streaming=False):
-        self.msg = messager
-        self.topic = topic
-        self.producer = producer
-        self.streaming = streaming
-
-    def print_message(self, message_raw):
-        message = json.loads(message_raw)
-        for a in message:
-            body = a["body"]
-
-            toc = a["body"].get("toc_id", "")
-            platform = a["body"].get("platform", "")
-            loc_stanox = "@" + body.get("loc_stanox", "")
-            timestamp = int(a["body"].get("actual_timestamp", 0)) / 1000
-            utc_datetime = datetime.utcfromtimestamp(timestamp)
-            uk_datetime = TIMEZONE_LONDON.fromutc(utc_datetime)
-            variation_status = a["body"].get("variation_status", "")
-
-            summary = "{} ({} {}) {:<13s} {:2s} {:<6s} {:3s} {} {}".format(
-                body["train_id"],
-                body["train_id"][2:6],
-                body["train_id"][6],
-                MESSAGES[a["header"]["msg_type"]],
-                toc,
-                loc_stanox,
-                platform,
-                uk_datetime,
-                variation_status,
-            )
-            # print(body)
-            print(summary)
-
-    def on_message(self, frame):
-        """Stomp message handler"""
-
-        headers, message_raw = frame.headers, frame.body
-
-        # Acknowledging messages is important in client-individual mode and keep a durable subscription
-        self.msg.ack(id=headers["message-id"], subscription=headers["subscription"])
-
-        if self.streaming:
-            # print(frame)
-            msg = json.loads(message_raw)
-            # print(msg)
-            # self.producer.produce(self.topic, json.dumps(msg))
-            for info in msg:
-                header = info["header"]
-                msg_type = header["msg_type"]
-                body = info["body"]
-                if msg_type == "0003":
-                    # print(type(body))
-                    train_records = self.producer.read_records([body])
-                    self.producer.publish(
-                        topic="train_movements", records=train_records
-                    )
-        else:
-            self.print_message(message_raw)
 
 
 class DataFeeder:
@@ -252,7 +76,7 @@ if __name__ == "__main__":
         topic="TRAIN_MVT_ALL_TOC",
         username=feed_username,
         password=feed_password,
-        listener=MVListener,
+        listener=MQListener,
         streaming=True,
         # streaming=False, # Use this option to preview data in the terminal
     )
